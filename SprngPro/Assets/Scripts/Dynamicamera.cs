f610cc6b741367433dd07a7a8dcfb7dc155e5f93
BugiNinja <lassi.tormikoski@gmail.com>
2018-03-21 12:20:27 +0200

    folders and cameratest

---
+++ SprngPro/Assets/Scripts/Dynamicamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamicamera : MonoBehaviour {
+
+    GameObject player;
+    Camera c;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("PLayer");
+        c = gameObject.GetComponent<Camera>();
+	}
+	
+
+	void Update () {
+		
+	}
+}
