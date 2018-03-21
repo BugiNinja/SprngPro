7384f9269757c755575d8674df7b0993bb1cc9f4
Yrjölä Mikko TTK17SP <mikkoyrjola@kamk.fi>
2018-03-21 12:10:49 +0200

    Placeholder art + Movement script

---
+++ SprngPro/Assets/Scenes/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed;
+    public float walkSpeed;
+    public float runSpeed;
+
+	void Start () {
+        walkSpeed = 3;
+        runSpeed = 6;
+        moveSpeed = walkSpeed;
+	}
+	
+	void Update ()
+    {
+        SwitchBackAndForthBetweenWalkAndRun();
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+	}
+
+    void SwitchBackAndForthBetweenWalkAndRun()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && moveSpeed != runSpeed)
+        {
+            moveSpeed = runSpeed;
+        }
+        else if(Input.GetKeyDown(KeyCode.R) && moveSpeed != walkSpeed)
+        {
+            moveSpeed = walkSpeed;
+        }
+    }
+}
