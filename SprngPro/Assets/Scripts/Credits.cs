8f2ca4cb391165be93bc745920a5be09fd1f8642
BugiNinja <lassi.tormikoski@gmail.com>
2018-05-28 13:08:43 +0300

    ending fix

---
+++ SprngPro/Assets/Scripts/Credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+    private SceneChange sc;
+	// Use this for initialization
+	void Start () {
+        sc = FindObjectOfType<SceneChange>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            sc.ChangeScene(0);
+        }
+	}
+}
---
+++ SprngPro/Assets/Scripts/endscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endscene : MonoBehaviour {
+    private SceneChange sc;
+    public bool end;
+	// Use this for initialization
+	void Start () {
+        sc = FindObjectOfType<SceneChange>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (end)
+        {
+            sc.ChangeScene(4);
+        }
+	}
+}
